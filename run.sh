8c91972ec56c05d0a67ca1a7f25b42da85db6aff
Brett Mack <brett@brettmack.me>
2019-07-16 02:55:46 +0100

    First commit

---
+++ run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+main() {
+  if [ "$WERCKER_YARN_INSTALL_USE_CACHE" == "true" ]; then
+    info "Using wercker cache"
+    setup_cache
+  fi
+
+  set +e
+  yarn_install
+  set -e
+
+  success "Finished yarn install"
+}
+
+setup_cache() {
+  debug 'Creating $WERCKER_CACHE_DIR/wercker/yarn'
+  mkdir -p "$WERCKER_CACHE_DIR/wercker/yarn"
+  
+  debug 'Configuring yarn to use wercker cache'
+  yarn config set cache-folder "$WERCKER_CACHE_DIR/wercker/yarn"
+}
+
+clear_cache() {
+  warn "Clearing yarn cache"
+  yarn cache clean
+  
+  # make sure the cache contains something, so it will override cache that get's stored
+  debug 'Creating $WERCKER_CACHE_DIR/wercker/yarn'
+  mkdir -p "$WERCKER_CACHE_DIR/wercker/yarn"
+  printf keep > "$WERCKER_CACHE_DIR/wercker/yarn/.keep"
+}
+
+yarn_install() {
+  local retries=3;
+  for try in $(seq "$retries"); do
+    info "Starting yarn install, try: $try"
+    yarn install $WERCKER_YARN_INSTALL_OPTIONS && return;
+
+    if [ "$WERCKER_YARN_INSTALL_CLEAR_CACHE_ON_FAILED" == "true" ]; then
+      clear_cache
+    fi
+  done
+
+  fail "Failed to successfully execute yarn install, retries: $retries"
+}
+
+main;
